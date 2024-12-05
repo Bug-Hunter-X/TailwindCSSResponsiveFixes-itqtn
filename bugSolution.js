d0cdf0b4ffcbbb88d9fc2fc907aed8c5eee3a954
BugHunter-X <askzenai@gmail.com>
2024-12-05 16:12:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,7 @@
+```javascript
+<div class="flex justify-center">
+  <div class="w-full sm:w-1/2 bg-gray-200 p-4">
+    <p>This text is now responsive and adjusts to smaller screens.</p>
+  </div>
+</div>
+```
